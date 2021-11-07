a05c756aeea02b3f19bc8f9482bb14724d861fc3
Alexandre Heine <xandeaph@gmail.com>
2021-11-06 21:08:08 -0300

    funções calcula e parte de roteia (falta conexão TCP)

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,6 +2,7 @@
 
 use std::net::{Shutdown, TcpListener, TcpStream};
 use std::io::Read;
+use std::fmt::Write;
 use std::process;
 use std::env;
 use std::thread;
@@ -50,7 +51,41 @@ fn get_mensagem(mut stream: TcpStream) ->String {
     return String::from_utf8_lossy(&buffer).to_string();
 }
 
-fn trata(mensagem: String) {
+fn calcula(chave: String, power2_nodes: i32) {
+    let node_quantity = i32::pow(2, &power2_nodes);
+    let mut soma = 0;
+
+    for byte in chave.as_bytes() {
+        let mut hexstring = String::new();
+        write!(&mut hexstring, "{:x}", &byte).expect("Unable to write"); // converte de byte para hexstring
+
+        let byte_int = match i32::from_str_radix(s.as_str(), 16) { // converte de hexstring para inteiro
+            Ok(num) => num,
+            Err(_) => {println!("invalid temp argument"); process::exit(0x01);}
+        };
+
+        soma += byte_int; // adiciona o byte ao somador
+    }
+
+    return soma % node_quantity; // calcula o hash
+}
+
+fn roteia(no: i32, no_atual: i32, power2_nodes: i32, chave: String, tipo: String, valor_ou_endereco: String) {
+    let mut no_prox; // próximo nó
+
+    for i in 0..power2_nodes-1 { // calcula o próximo nó a ser visitado
+        let no_verifica = no_atual + i32::pow(2, &i);
+        if no > no_pow { // se o nó verificado é menor que o destino, então deve-se ir ao no verificado anteriormente
+            break;
+        }
+        no_prox = no_verifica;
+    }
+
+    // faz conexão TCP com o próximo nó
+
+}
+
+fn trata(mensagem: String, node: i32, power2_nodes: i32) {
     /// Trata a mensagem, executando as funções abaixo para cada caso
     ///   "insere--chave--valor" -> chama *calcula*, e depois *roteia* ou *coloca*
     ///   "insere_no--chave--valor--no" -> chama *roteia* ou *coloca*
@@ -65,8 +100,8 @@ fn trata(mensagem: String) {
     // decide para onde ir
     if m.starts_with("insere--") {
         if v.len() != 3 {println!("mensagem de insercao invalida !"); return;}
-        // let chave = v[1].to_string();
-        // let valor = v[2].to_string();
+        let chave = v[1].to_string();
+        let valor = v[2].to_string();
         // TODO: chamar calcula, e depois roteia ou coloca
     }
     else if m.starts_with("insere_no") {
@@ -154,12 +189,13 @@ fn loop_hash(receiver: Receiver<SendHash>) {
 fn main() {
     // verificando node e portas
     let args: Vec<String> = env::args().collect();
-    if args.len() < 3 {
-        println!("usage: {} node_number port", &args[0]);
+    if args.len() < 4 {
+        println!("usage: {} node_number port power2_nodes", &args[0]);
         process::exit(0x01);
     }
     let temp_node = &args[1].parse::<i32>();
     let temp_port = &args[2].parse::<i32>();
+    let temp_power2_nodes = &args[3].parse::<i32>();
 
     let node = match temp_node {
         Ok(num) => num,
@@ -168,6 +204,10 @@ fn main() {
     let port = match temp_port {
         Ok(num) => num,
         Err(_) => {println!("invalid port argument"); process::exit(0x01);}
+    };
+    let power2_nodes = match temp_power2_nodes {
+        Ok(num) => num,
+        Err(_) => {println!("invalid power2_nodes argument"); process::exit(0x01);}
     };
 
     // criando o hash
@@ -186,6 +226,6 @@ fn main() {
         let mensagem = get_mensagem(stream);
 
         // trata a mensagem
-        thread::spawn(move || {trata(mensagem); });
+        thread::spawn(move || {trata(mensagem, node, power2_nodes); });
     }
 }
